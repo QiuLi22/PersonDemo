4b04844dad190c8b22c28c0f4aa17899b3bbd5be
QiuLi22 <104408128+QiuLi22@users.noreply.github.com>
2022-05-26 22:38:38 -0700

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDemo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public void DisplayName()
+        {
+            Console.WriteLine($"{LastName}, {FirstName}");
+        }
+
+        public void DisplayEmail()
+        {
+            Console.WriteLine($"{Email}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Qiu Li 5-26-2022  
+
+using PersonDemo;
+
+Person personA = new Person();
+personA.FirstName = "Qiu";
+personA.LastName = "Li";
+personA.Email = "qiuli@stmc.edu";
+
+Person personB = new Person();
+personB.FirstName = "Raque";
+personB.LastName = "Li";
+personB.Email = "r.Li@stmc.edu";
+
+Person personC = new Person();
+personC.FirstName = "Charley";
+personC.LastName = "Li";
+personC.Email = "charley.li@stmc.edu";
+
+personA.DisplayName();
+personA.DisplayEmail();
+personB.DisplayName();
+personB.DisplayEmail();
+personC.DisplayName();
+personC.DisplayEmail();
+
+
+
+
+
